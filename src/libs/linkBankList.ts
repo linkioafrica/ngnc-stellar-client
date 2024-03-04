d440164d054def35a177a96d9bea3def3ecf1780
Tomisin Leshi <tom-lesh@outlook.com>
2024-03-04 15:59:19 +0100

    Minor update

--- src/libs/linkBankList.ts
+++ src/libs/linkBankList.ts
@@ -2,36 +2,36 @@ export const linkBankList = [
   {
     accountName: "Christopher Okoye",
     accountNumber: "6153077102",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "marvel ekpo",
     accountNumber: " 6154172695",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "divine-will ekpo",
     accountNumber: "6154346014",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "Okemdinachi Nwaejie ",
     accountNumber: "6156824613",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "emmanuel evarist",
     accountNumber: "6156789026",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "victor onyezia",
     accountNumber: "6154883023",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
   {
     accountName: "Tansi Jones",
     accountNumber: "6158186966",
-    bankName: "9Payment Service Bank",
+    bankName: "9Payment Service",
   },
 ];
