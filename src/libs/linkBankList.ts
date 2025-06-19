df7e99c81266bed0d25d30428c22156600b5cfab
Tomisin Leshi <tom-lesh@outlook.com>
2025-06-19 14:56:21 +0100

    Update linkBankList.ts

--- src/libs/linkBankList.ts
+++ src/libs/linkBankList.ts
@@ -1,32 +1,37 @@
 export const linkBankList = [
+  // {
+  //   accountName: "PASSPOINT (Okemdinachi Nwaeji)",
+  //   accountNumber: "9636369057",
+  //   bankName: "Providus Bank",
+  // },
+  // {
+  //   accountName: "PASSPOINT (christopher okoye)",
+  //   accountNumber: "9631525849",
+  //   bankName: "Providus Bank",
+  // },
+  // {
+  //   accountName: "PASSPOINT (marvel ekpo)",
+  //   accountNumber: " 9628442937",
+  //   bankName: "Providus Bank",
+  // },
+  // {
+  //   accountName: "PASSPOINT (divine-will ekpo)",
+  //   accountNumber: "9637681398",
+  //   bankName: "Providus Bank",
+  // },
+  // {
+  //   accountName: "PASSPOINT (emmanuel evarist)",
+  //   accountNumber: "9618182180",
+  //   bankName: "Providus Bank",
+  // },
+  // {
+  //   accountName: "PASSPOINT (victor onyezia)",
+  //   accountNumber: "9611298145",
+  //   bankName: "Providus Bank",
+  // },
   {
-    accountName: "PASSPOINT (Okemdinachi Nwaeji)",
-    accountNumber: "9636369057",
-    bankName: "Providus Bank",
-  },
-  {
-    accountName: "PASSPOINT (christopher okoye)",
-    accountNumber: "9631525849",
-    bankName: "Providus Bank",
-  },
-  {
-    accountName: "PASSPOINT (marvel ekpo)",
-    accountNumber: " 9628442937",
-    bankName: "Providus Bank",
-  },
-  {
-    accountName: "PASSPOINT (divine-will ekpo)",
-    accountNumber: "9637681398",
-    bankName: "Providus Bank",
-  },
-  {
-    accountName: "PASSPOINT (emmanuel evarist)",
-    accountNumber: "9618182180",
-    bankName: "Providus Bank",
-  },
-  {
-    accountName: "PASSPOINT (victor onyezia)",
-    accountNumber: "9611298145",
+    accountName: "DORBEN MFB(Linkio. Global Ser)",
+    accountNumber: "9640460870",
     bankName: "Providus Bank",
   },
 ];
